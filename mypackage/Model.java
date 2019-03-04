f09982bae1ccd0558619a918d949e57e48f1382f
suint <lui.ysia@gmail.com>
2019-03-04 18:40:57 -0500

    stubs, added imgs

--- mypackage/Model.java
+++ mypackage/Model.java
@@ -10,5 +10,30 @@ package mypackage;
  * provide location
  **/
 class Model{
+	int X;
+	int Y;
+	
+	
+	Model(int width, int height, int imageWidth, int imageHeight){
+		this.X = width;
+		this.Y = height;
+		//idk where these would go? are they fields
+	}
+	
+	public int getX() {
+		return 0;
+	}
+	
+	public int getY() {
+		return 0;
+	}
+	
+	public Direction getDirection() {
+		return Direction.NORTH;
+	}
+	
+	public void updateDirectionAndLocation() {
+		X = 12;
+	}
 	
 }
--- mypackage/View.java
+++ mypackage/View.java
@@ -9,5 +9,16 @@ package mypackage;
  * load images for all direction (an image should only be loaded once!!! why?)
  **/
 class View{
-	
+	public int getWidth(){
+		return 0;
+	}
+	public int getHeight() {
+		return 0;
+	}
+	public int getImageWidth() {
+		return 0;
+	}
+	public int getImageHeight() {
+		return 0;
+	}
 }
